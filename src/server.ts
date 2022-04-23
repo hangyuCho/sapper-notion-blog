155f0837527e990f974daf776a3f74ee9102f4c3
hangyuCho <chg3376@gmail.com>
2022-04-24 08:47:26 +0900

    first commit

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import sirv from 'sirv';
+import polka from 'polka';
+import compression from 'compression';
+import * as sapper from '@sapper/server';
+
+const { PORT, NODE_ENV } = process.env;
+const dev = NODE_ENV === 'development';
+const options: compression.CompressionOptions = { threshold: 0 }
+
+polka() // You can also use Express
+	.use(
+		compression(options),
+		sirv('static', { dev }),
+		sapper.middleware()
+	)
+	.listen(PORT, err => {
+		if (err) console.log('error', err);
+	});
